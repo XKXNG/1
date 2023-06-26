af8f1ad9c8245c5fbcce6938a993e8f631009a7b
XKXNG <137825498+XKXNG@users.noreply.github.com>
2023-06-26 14:44:59 -0400

    Add files via upload

---
+++ waterfox.py
@@ -0,0 +1,10 @@
+import os
+import time
+print("Is Waterfox Installed?")
+water = input("y/n: ")
+if water == "n":
+	os.system('wget https://cdn1.waterfox.net/waterfox/releases/G5.1.8/Linux_x86_64/waterfox-G5.1.8.tar.bz2')
+	time.sleep(5)
+	os.system('tar -xf waterfox-G5.1.8.tar.bz2')
+else:
+	os.system('cd Downloads && cd waterfox && ./waterfox')
