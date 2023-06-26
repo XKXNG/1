3c89919134cbbf566941dfd6190baeebdbfeda00
XKXNG <137825498+XKXNG@users.noreply.github.com>
2023-06-26 16:56:13 -0400

    Update waterfox.py

--- waterfox.py
+++ waterfox.py
@@ -5,6 +5,6 @@ water = input("y/n: ")
 if water == "n":
 	os.system('wget https://cdn1.waterfox.net/waterfox/releases/G5.1.8/Linux_x86_64/waterfox-G5.1.8.tar.bz2')
 	time.sleep(5)
-	os.system('tar -xf waterfox-G5.1.8.tar.bz2')
+	os.system('tar -xvf waterfox-G5.1.8.tar.bz2')
 else:
 	os.system('cd Downloads && cd waterfox && ./waterfox')
